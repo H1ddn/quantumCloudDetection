8669800cdc2d17a306affd16af6a992a7a4db68d
Ethan Cruz <43393590+H1ddn@users.noreply.github.com>
2024-03-27 13:50:26 -0400

    Added Comments
    Included comments to code

--- preprocess.py
+++ preprocess.py
@@ -1,33 +1,41 @@
 from pyhdf.SD import SD, SDC
+from pyhdf.HDF import HDF, HC
+from pyhdf.VS import *
 import os
 import numpy as np
-from pyhdf.HDF import *
-from pyhdf.VS import *
 from scipy.spatial import cKDTree
 import csv
 
-def build_tree(lat1, lon1):
-    """
-    Build a KDTree from latitude and longitude arrays.
-    
-    Parameters:
-        lat1 (ndarray): Latitude array.
-        lon1 (ndarray): Longitude array.
-    
-    Returns:
-        tree: KDTree object.
-    """
-    return cKDTree(np.column_stack((lat1, lon1)))
+def build_tree(lat, lon):
+    return cKDTree(np.column_stack((lat, lon)))
 
-def co_locate(tree, lat2, lon2, threshold):
+def co_locate(tree, lat1, lon1, lat2, lon2, threshold):
     co_located_indices = []
     
-    for i, (lat, lon) in enumerate(zip(lat2, lon2)):
-        dist, idx = tree.query([lat, lon])
+    for i, (lat1, lon1) in enumerate(zip(lat2, lon2)):
+        dist, idx = tree.query([lat1, lon1])
         if dist <= threshold:
             co_located_indices.append((idx, i))
             
     return co_located_indices
+
+def calcRadiance(data):
+    emissive = data[:]
+    # Get dataset attributes
+    attrs = data.attributes()
+    
+    # Apply scaling and offset for each channel
+    radiance = np.empty(emissive.shape, dtype=np.float32)
+    
+    for i in range(emissive.shape[0]):
+        channel_values = emissive[i, :, :]
+        channel_radiance = ((channel_values.astype(np.float32) - attrs['radiance_offsets'][i]) /
+                            attrs['radiance_scales'][i])
+        # Handle special values
+        channel_radiance[channel_values.astype(np.float32) == attrs['_FillValue']] = np.nan
+        radiance[i, :, :] = channel_radiance
+
+    return radiance
 
 # Get the list of files in both directories
 filesCC = sorted(os.listdir("CloudSatCALIPSO"))
@@ -43,49 +51,28 @@ for i in range(size):
     filenameCC = os.path.join("CloudSatCALIPSO",filesCC.pop(0))
     print(filenameCC)
 
-    # Open the HDF file in read mode
+    # Open HDF
     hdf_file = HDF(filenameCC, HC.READ)
 
-    # Open the Vdata interface
+    # Open Vdata interface
     vdata = hdf_file.vstart()
 
-    # Initialize latitude and longitude data
-    latitude_data = None
-    longitude_data = None
+    field_ref = vdata.attach('Latitude')
+    latitude_data_cc = field_ref[:]
+    field_ref.detach()
 
-    # Get the list of Vdata fields
-    vdata_info = vdata.vdatainfo()
+    field_ref = vdata.attach('Longitude')
+    longitude_data_cc = field_ref[:]
+    field_ref.detach()
 
-    # Iterate through each Vdata field
-    for index, info in enumerate(vdata_info):
-        try:
-            field_name = info[0]  # Name of the Vdata field
-            
-            # Check if the Vdata field contains latitude or longitude data
-            if field_name == 'Latitude':
-                field_ref = vdata.attach(field_name)  # Reference to the Vdata field
-                latitude_data_cc = field_ref[:]
-                # Detach from the current Vdata field
-                field_ref.detach()
-            elif field_name == 'Longitude':
-                field_ref = vdata.attach(field_name)  # Reference to the Vdata field
-                longitude_data_cc = field_ref[:]
-                # Detach from the current Vdata field
-                field_ref.detach()
-            elif field_name == 'Cloudlayer':
-                field_ref = vdata.attach(field_name)  # Reference to the Vdata field
-                cloud_layer = field_ref[:]
-                cloud_layer = np.ravel(cloud_layer)
-                # Detach from the current Vdata field
-                field_ref.detach()
-            
-        except HDF4Error as e:
-            print(f"Error attaching Vdata field at index {index}: {e}")
+    field_ref = vdata.attach('Cloudlayer')
+    cloud_layer = field_ref[:]
+    field_ref.detach()
 
-    # Close the Vdata interface
+    # Close Vdata interface
     vdata.end()
 
-    # Close the HDF file
+    # Close HDF
     hdf_file.close()
             
     longitude_data_cc = np.ravel(longitude_data_cc)
@@ -99,113 +86,56 @@ for i in range(size):
             fnMOD = filesMOD.pop(0)
             # Take one file from directory2
             filenameMOD = os.path.join("MODIS_raw",fnMOD)
-            # Open the HDF file in read mode
+           # Open HDF
             hdf_file = SD(filenameMOD, SDC.READ)
 
-            # List SDS datasets in the file
+            # List SDS datasets
             sds_datasets = hdf_file.datasets()
 
-            # Iterate through SDS datasets
-            for dataset_name in sds_datasets:
-                # print("SD dataset name:", dataset_name)
-                # Check if the dataset contains geolocation information
-                if 'Latitude' in dataset_name:
-                    latitude_data_mod = hdf_file.select(dataset_name)[:]
-                elif 'Longitude' in dataset_name:
-                    longitude_data_mod = hdf_file.select(dataset_name)[:]
-                elif 'EV_1KM_Emissive' == dataset_name:
-                    data = hdf_file.select(dataset_name)
-                    ev_1km_emissive = data[:]
-                    
-                    # Get dataset attributes
-                    attrs = data.attributes()
+            latitude_data_mod = hdf_file.select('Latitude')[:]
+            longitude_data_mod = hdf_file.select('Longitude')[:]
 
-                    # Apply scaling and offset for each channel
-                    radiance_1km = np.empty(ev_1km_emissive.shape, dtype=np.float32)
-                    
-                    for i in range(ev_1km_emissive.shape[0]):
-                        channel_values = ev_1km_emissive[i, :, :]
-                        channel_radiance = ((channel_values.astype(np.float32) - attrs['radiance_offsets'][i]) /
-                                            attrs['radiance_scales'][i])
-                        # Handle special values
-                        channel_radiance[channel_values.astype(np.float32) == attrs['_FillValue']] = np.nan
-                        radiance_1km[i, :, :] = channel_radiance
-                elif 'EV_1KM_RefSB' == dataset_name:
-                    data = hdf_file.select(dataset_name)
-                    ev_1km_refsb = data[:]
-                    
-                    # Get dataset attributes
-                    attrs = data.attributes()
-
-                    # Apply scaling and offset for each channel
-                    refradiance_1km = np.empty(ev_1km_refsb.shape, dtype=np.float32)
-                    
-                    for i in range(ev_1km_refsb.shape[0]):
-                        channel_values = ev_1km_refsb[i, :, :]
-                        channel_radiance = ((channel_values.astype(np.float32) - attrs['radiance_offsets'][i]) /
-                                            attrs['radiance_scales'][i])
-                        # Handle special values
-                        channel_radiance[channel_values.astype(np.float32) == attrs['_FillValue']] = np.nan
-                        refradiance_1km[i, :, :] = channel_radiance
-                elif 'EV_250_Aggr1km_RefSB' == dataset_name:
-                    data = hdf_file.select(dataset_name)
-                    ev_250_refsb = data[:]
-                    
-                    # Get dataset attributes
-                    attrs = data.attributes()
-
-                    # Apply scaling and offset for each channel
-                    refradiance_250 = np.empty(ev_250_refsb.shape, dtype=np.float32)
-                    
-                    for i in range(ev_250_refsb.shape[0]):
-                        channel_values = ev_250_refsb[i, :, :]
-                        channel_radiance = ((channel_values.astype(np.float32) - attrs['radiance_offsets'][i]) /
-                                            attrs['radiance_scales'][i])
-                        # Handle special values
-                        channel_radiance[channel_values.astype(np.float32) == attrs['_FillValue']] = np.nan
-                        refradiance_250[i, :, :] = channel_radiance
-                elif 'EV_500_Aggr1km_RefSB' == dataset_name:
-                    data = hdf_file.select(dataset_name)
-                    ev_500_refsb = data[:]
-                    
-                    # Get dataset attributes
-                    attrs = data.attributes()
-
-                    # Apply scaling and offset for each channel
-                    refradiance_500 = np.empty(ev_500_refsb.shape, dtype=np.float32)
-                    
-                    for i in range(ev_500_refsb.shape[0]):
-                        channel_values = ev_500_refsb[i, :, :]
-                        channel_radiance = ((channel_values.astype(np.float32) - attrs['radiance_offsets'][i]) /
-                                            attrs['radiance_scales'][i])
-                        # Handle special values
-                        channel_radiance[channel_values.astype(np.float32) == attrs['_FillValue']] = np.nan
-                        refradiance_500[i, :, :] = channel_radiance
+            radiance_1km = calcRadiance(hdf_file.select('EV_1KM_Emissive'))
+            refradiance_1km = calcRadiance(hdf_file.select('EV_1KM_RefSB'))
+            refradiance_250 = calcRadiance(hdf_file.select('EV_250_Aggr1km_RefSB'))
+            refradiance_500 = calcRadiance(hdf_file.select('EV_500_Aggr1km_RefSB'))
 
             # Close the HDF file
             hdf_file.end()
+
+            # Join radiances into feature space
             features = np.concatenate((radiance_1km, refradiance_1km, refradiance_250, refradiance_500), axis=0)
+
+            # Grab middle lat/lons
             longitude_data_mod = np.ravel(longitude_data_mod[:,1])
             latitude_data_mod = np.ravel(latitude_data_mod[:,1])
+
+            # Colocate CloudSat CALIPSO
             co_located_indices = co_locate(tree, latitude_data_mod, longitude_data_mod, 0.025)
             cloud_layer_colocated = np.array([cloud_layer[idx[0]] for idx in co_located_indices])
             cloud_layer_colocated = np.reshape(cloud_layer_colocated, (-1, 1))
+
+            # Grab middle features
             features = features[:,2::5,5]
+
+            # Colocate MODIS
             features_colocated = [features[:,idx[1]] for idx in co_located_indices]
 
+            # If nothing is colocated, reset search on the next cloudsat calipso file
             if len(features_colocated) == 0:
                 filesMOD.insert(0, fnMOD)
                 break
 
+            # Join array
             concatenated_array = np.concatenate((cloud_layer_colocated, features_colocated), axis=1)
 
-            # Path to the existing CSV file
+            # Pato CSV file
             existing_csv_file = "data.csv"
 
-            # Save the NumPy array to a temporary CSV file
+            # Save NumPy array to temp CSV file
             np.savetxt("temp_array.csv", concatenated_array, delimiter=",")
 
-            # Append the temporary CSV file to the existing CSV file
+            # Append temp CSV file to existing CSV file
             with open(existing_csv_file, 'a', newline='') as f:
                 writer = csv.writer(f)
                 with open("temp_array.csv", 'r') as temp_file:
@@ -213,7 +143,7 @@ for i in range(size):
                     for row in reader:
                         writer.writerow(row)
 
-            # Optionally, you can remove the temporary CSV file
+            # Remove temp CSV file
             import os
             os.remove("temp_array.csv")
 
--- processCloudMask.py
+++ processCloudMask.py
@@ -1,29 +1,19 @@
 from pyhdf.SD import SD, SDC
+from pyhdf.HDF import HDF, HC
+from pyhdf.VS import *
 import os
 import numpy as np
-from pyhdf.HDF import *
-from pyhdf.VS import *
 from scipy.spatial import cKDTree
 import csv
 
-def build_tree(lat1, lon1):
-    """
-    Build a KDTree from latitude and longitude arrays.
-    
-    Parameters:
-        lat1 (ndarray): Latitude array.
-        lon1 (ndarray): Longitude array.
-    
-    Returns:
-        tree: KDTree object.
-    """
-    return cKDTree(np.column_stack((lat1, lon1)))
+def build_tree(lat, lon):
+    return cKDTree(np.column_stack((lat, lon)))
 
-def co_locate(tree, lat2, lon2, threshold):
+def co_locate(tree, lat1, lon1, lat2, lon2, threshold):
     co_located_indices = []
     
-    for i, (lat, lon) in enumerate(zip(lat2, lon2)):
-        dist, idx = tree.query([lat, lon])
+    for i, (lat1, lon1) in enumerate(zip(lat2, lon2)):
+        dist, idx = tree.query([lat1, lon1])
         if dist <= threshold:
             co_located_indices.append((idx, i))
             
@@ -43,49 +33,28 @@ for i in range(size):
     filenameCC = os.path.join("CloudSatCALIPSO",filesCC.pop(0))
     print(filenameCC)
 
-    # Open the HDF file in read mode
+    # Open HDF
     hdf_file = HDF(filenameCC, HC.READ)
 
-    # Open the Vdata interface
+    # Open Vdata interface
     vdata = hdf_file.vstart()
 
-    # Initialize latitude and longitude data
-    latitude_data = None
-    longitude_data = None
+    field_ref = vdata.attach('Latitude')
+    latitude_data_cc = field_ref[:]
+    field_ref.detach()
 
-    # Get the list of Vdata fields
-    vdata_info = vdata.vdatainfo()
+    field_ref = vdata.attach('Longitude')
+    longitude_data_cc = field_ref[:]
+    field_ref.detach()
 
-    # Iterate through each Vdata field
-    for index, info in enumerate(vdata_info):
-        try:
-            field_name = info[0]  # Name of the Vdata field
-            
-            # Check if the Vdata field contains latitude or longitude data
-            if field_name == 'Latitude':
-                field_ref = vdata.attach(field_name)  # Reference to the Vdata field
-                latitude_data_cc = field_ref[:]
-                # Detach from the current Vdata field
-                field_ref.detach()
-            elif field_name == 'Longitude':
-                field_ref = vdata.attach(field_name)  # Reference to the Vdata field
-                longitude_data_cc = field_ref[:]
-                # Detach from the current Vdata field
-                field_ref.detach()
-            elif field_name == 'Cloudlayer':
-                field_ref = vdata.attach(field_name)  # Reference to the Vdata field
-                cloud_layer = field_ref[:]
-                cloud_layer = np.ravel(cloud_layer)
-                # Detach from the current Vdata field
-                field_ref.detach()
-            
-        except HDF4Error as e:
-            print(f"Error attaching Vdata field at index {index}: {e}")
+    field_ref = vdata.attach('Cloudlayer')
+    cloud_layer = field_ref[:]
+    field_ref.detach()
 
-    # Close the Vdata interface
+    # Close Vdata interface
     vdata.end()
 
-    # Close the HDF file
+    # Close HDF
     hdf_file.close()
             
     longitude_data_cc = np.ravel(longitude_data_cc)
@@ -99,34 +68,28 @@ for i in range(size):
             fnMOD = filesMOD.pop(0)
             # Take one file from directory2
             filenameMOD = os.path.join("MODIS_cloudMask",fnMOD)
-            # Open the HDF file in read mode
+            # Open HDF
             hdf_file = SD(filenameMOD, SDC.READ)
 
-            # List SDS datasets in the file
-            sds_datasets = hdf_file.datasets()
 
-            # Iterate through SDS datasets
-            for dataset_name in sds_datasets:
-                # print("SD dataset name:", dataset_name)
-                # Check if the dataset contains geolocation information
-                if 'Latitude' in dataset_name:
-                    latitude_data_mod = hdf_file.select(dataset_name)[:]
-                elif 'Longitude' in dataset_name:
-                    longitude_data_mod = hdf_file.select(dataset_name)[:]
-                elif 'Cloud_Mask' == dataset_name:
-                    data = hdf_file.select(dataset_name)
-                    dataarray = data[:]
-                    dataarray = dataarray[0,:,5]
-
-                    binary_data = [format(byte, '08b') for byte in dataarray]
-                    extracted_bits = [byte[-7:-5] for byte in binary_data]
-                    cloud_mask_mod = [1 if bits in ['00', '01'] else 0 for bits in extracted_bits]
-
-            # Close the HDF file
+            latitude_data_mod = hdf_file.select('Latitude')[:]
+            longitude_data_mod = hdf_file.select('Longitude')[:]
+    
+            # Grab 0th end byte
+            dataarray = hdf_file.select('Cloud_Mask')[0,:,5]
+            # Gather data as Bytes
+            binary_data = [format(byte & 0xFF, '08b') for byte in dataarray]
+            # Gather 6th and 7th bit from right
+            extracted_bits = [byte[-7:-5] for byte in binary_data]
+            # Cloud mask information, simplify to [1,0]
+            cloud_mask_mod = [1 if bits in ['00', '01'] else 0 for bits in extracted_bits]
+                    
+            # Close HDF
             hdf_file.end()
+
             longitude_data_mod = np.ravel(longitude_data_mod[:,1])
             latitude_data_mod = np.ravel(latitude_data_mod[:,1])
-            co_located_indices = co_locate(tree, latitude_data_mod, longitude_data_mod, 0.025)
+            co_located_indices = co_locate(tree, latitude_data_cc, longitude_data_cc, latitude_data_mod, longitude_data_mod, 0.025)
             cloud_layer_colocated = np.array([cloud_layer[idx[0]] for idx in co_located_indices])
             cloud_layer_colocated = np.reshape(cloud_layer_colocated, (-1, 1))
             cloud_mask_mod_colocated = [cloud_mask_mod[idx[1]] for idx in co_located_indices]
@@ -138,13 +101,13 @@ for i in range(size):
 
             concatenated_array = np.concatenate((cloud_layer_colocated, cloud_mask_mod_colocated), axis=1)
 
-            # Path to the existing CSV file
+            # Path CSV file
             existing_csv_file = "modMaskData.csv"
 
-            # Save the NumPy array to a temporary CSV file
+            # Save NumPy array to temp CSV file
             np.savetxt("temp_array.csv", concatenated_array, delimiter=",")
 
-            # Append the temporary CSV file to the existing CSV file
+            # Append temp CSV file to existing CSV file
             with open(existing_csv_file, 'a', newline='') as f:
                 writer = csv.writer(f)
                 with open("temp_array.csv", 'r') as temp_file:
@@ -152,7 +115,7 @@ for i in range(size):
                     for row in reader:
                         writer.writerow(row)
 
-            # Optionally, you can remove the temporary CSV file
+            # Remove temp CSV file
             import os
             os.remove("temp_array.csv")
 
